0fb82575742ba3cc826761cabed9fb9d5b994881
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-09 08:53:55 +0300

    solved errors

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
-
-# Register your models here.
+# from django.contrib import admin
+# from .models import Editor,profile,Post
+# #Register  your models here .
+#
+# admin.site.register(Editor)
+# admin.site.register(profile)
+# admin.site.register(Post)
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+@login_required(login_url='/accounts/login/')
+def post(request,post_id):
+    try:
+        post = Post.objects.get(id = post_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"post.html", {"post":post})
