490f32255edd547bc157653424e011baec6108b6
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-12 12:52:12 +0300

    added readme

--- instagram/views.py
+++ instagram/views.py
@@ -4,14 +4,29 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/accounts/login/')
-def post(request,post_id):
+def post(request):
     try:
-        post = Post.objects.get(id = post_id)
+        post = Post.objects.all()
     except DoesNotExist:
         raise Http404()
     return render(request,"post.html", {"post":post})
 
+@login_required(login_url='/accounts/login')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.editor = current_user
+            post.save()
+        return redirect('NewsToday')
+    else:
+        form = PostForm()
+    return render(request, 'new_post.html', {"form": form})
+
+
 @login_required(login_url='/accounts/login/')
 def find(request, name):
     results = Profile.find_profile(name)
-    return render(request, 'searchresults.html', locals())
+    return render(request, 'search.html', locals())
