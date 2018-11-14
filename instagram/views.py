e5e20898cf1b6af5474f06e9e6369d9868a3bb76
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-14 16:24:27 +0300

    made more views

--- instagram/views.py
+++ instagram/views.py
@@ -13,36 +13,66 @@ def post(request):
     except DoesNotExist:
         raise Http404()
     return render(request,"post.html")
-#
-# @login_required(login_url='/accounts/login')
-# def new_post(request):
-#     current_user = request.user
-#     if request.method == 'POST':
-#         form = PostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             post = form.save(commit=False)
-#             post.editor = current_user
-#             post.save()
-#         return redirect('NewsToday')
-#     else:
-#         form = PostForm()
-#     return render(request, 'new_post.html', {"form": form})
-#
-#
-# @login_required(login_url='/accounts/login/')
-# def find(request, name):
-#     results = profile.find_profile(name)
-#     return render(request, 'search.html', locals())
-#
-# @login_required(login_url='/accounts/login/')
-# def comment_on(request, post_id):
-#     commentform = CommentForm()
-#     post = get_object_or_404(Post, pk=post_id)
-#     if request.method == 'POST':
-#         form = CommentForm(request.POST)
-#         if form.is_valid():
-#             comment = form.save(commit=False)
-#             comment.user = request.user.profile
-#             comment.photo = post
-#             comment.save()
-#     return render(request, 'post.html', locals())
+
+def profile(request):
+    current_user = request.user
+    image = Image.objects.filter(profile = current_user)
+
+    try:
+        # profile = get_object_or_404(Profile,user=current_user)
+        profile = Profile.objects.get(user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('welcome')
+    print(profile.bio)
+    return render(request,'profile.html',{ 'profile':profile,'image':image,'current_user':current_user})
+
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.profile = current_user
+            profile.save()
+        return redirect('profile')
+    else:
+        form = NewProfileForm()
+    return render(request,'edit_profile.html', {'form':form})
+
+def search_results(request):
+
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_profile = Profile.search_by_username(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-insta/search.html',{"message":message,"profiles": searched_profile})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-insta/search.html',{"message":message})
+
+def search_profile(request,profile_id):
+    try :
+        profile = Profile.objects.get(id = profile_id)
+
+    except ObjectDoesNotExist:
+        # raise Http404()
+        return render(request, 'all-insta/no_profile.html')
+
+    return render(request, 'all-insta/search_profile.html', {'profile':profile})
+
+def comment_photo(request, image_id):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewCommentForm(request.POST, request.FILES)
+        image = get_object_or_404(Image, pk = image_id)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.comment = current_user
+            comment.image = image
+            comment.save()
+        return redirect('comment-photo')
+    else:
+        form = NewCommentForm()
+    return render(request,'comment.html', {'form':form})
